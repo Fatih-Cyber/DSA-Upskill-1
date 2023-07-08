454f929d1f6a023cfd92948030d5729ba2aaa68b
Fatih-Cyber <fatih@cybertekschool.com>
2023-07-08 17:30:53 +0200

    Linked Lists Review

--- dsa-04-LinkedLists/src/main/java/Review3_LinkedLists/UserLinkedList.java
+++ dsa-04-LinkedLists/src/main/java/Review3_LinkedLists/UserLinkedList.java
@@ -13,11 +13,27 @@ public class UserLinkedList {
         // Case 1: List is Empty
         if (isEmpty()) {
            head=tail=node;
-        } else
-        { // Case 2: Not empty
-            tail.next=node;
+        } else {
+            tail.next=node;// Case 2: Not empty
             tail=node;
         }
         size++;
+    }
+
+    void printNames(){
+        // first check if list is empty
+        if (isEmpty()) {
+            System.out.println("List is empty !!!!");
+            return;
+        }
+        // Go to the first element in the list
+        UserNode current=head;
+        System.out.println();
+        while (current!=null) {
+            System.out.print(current.name+ " => ");
+            current=current.next;
+            if (current==null) System.out.print("null");
+        }
+
     }
 }
--- dsa-04-LinkedLists/src/main/java/Review3_LinkedLists/UserListApp.java
+++ dsa-04-LinkedLists/src/main/java/Review3_LinkedLists/UserListApp.java
@@ -1,4 +1,9 @@
 package Review3_LinkedLists;
 
 public class UserListApp {
+    public static void main(String[] args) {
+        UserNode node1=new UserNode("Alexander","Smith");
+        UserLinkedList myLinkedListSingle=new UserLinkedList();
+        myLinkedListSingle.insertLast(node1);
+    }
 }
