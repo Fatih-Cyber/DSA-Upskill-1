fa06ae89e0feb445cfa6ba86fee924896f7fcb9a
Fatih-Cyber <fatih@cybertekschool.com>
2023-07-16 17:33:48 +0200

    Leetcode problem 1472 Design Browser History

--- dsa-05-Stacks/src/main/java/Review4/BrowserHistory.java
+++ dsa-05-Stacks/src/main/java/Review4/BrowserHistory.java
@@ -1,21 +1,45 @@
 package Review4;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class BrowserHistory {
-
+    Deque<String> history, forwardStack;
+    String currentPage;
     public BrowserHistory(String homepage) {
-
+       // Initialize stacks and set current Url to Homepage
+         history=new ArrayDeque<>();
+         forwardStack=new ArrayDeque<>();
+         currentPage=homepage;
     }
 
     public void visit(String url) {
-
+      history.push(currentPage);
+      System.out.println(url + " is visited");
+      currentPage=url;
+      forwardStack=new ArrayDeque<>();
     }
 
     public String back(int steps) {
-      return "url";
+        int stepNumber=steps;
+        while(steps>0 && !history.isEmpty()){
+            forwardStack.push(currentPage);
+            currentPage= history.pop();
+            steps--;
+        }
+        System.out.println("Back operation of "+ stepNumber + " brings " + currentPage + " as current page");
+      return currentPage;
     }
 
     public String forward(int steps) {
-        return "url";
+        int stepNumber=steps;
+        while(steps>0 && !forwardStack.isEmpty()){
+            history.push(currentPage);
+            currentPage= forwardStack.pop();
+            steps--;
+        }
+        System.out.println("Forward operation of "+ stepNumber + " brings " + currentPage + " as current page");
+        return currentPage;
     }
 
 
