74072fbd32b9a13991668ebb9a99c9811f332504
Fatih-Cyber <fatih@cybertekschool.com>
2023-07-23 17:19:52 +0200

    Key Value added

---
+++ dsa-06-Queues/src/main/java/Review5_Queues_Hashing/KeyValue.java
@@ -0,0 +1,11 @@
+package Review5_Queues_Hashing;
+
+public class KeyValue<U,V> {
+    U key;
+    V value;
+
+    public KeyValue(U key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
