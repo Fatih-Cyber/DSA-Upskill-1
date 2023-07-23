c44ddffa5af21da8dc4863d5ad3b10ff2d109bde
Fatih-Cyber <fatih@cybertekschool.com>
2023-07-23 18:18:44 +0200

    Heap and tree added

--- dsa-07-Trees/src/main/java/TreeApp.java
+++ dsa-07-Trees/src/main/java/TreeApp.java
@@ -18,13 +18,7 @@ public class TreeApp {
         System.out.println("Level Order Traversal: ");
         tree.levelOrderTraversal();*/
 
-        tree.printLeaves(tree.root);
-        System.out.println();
-        System.out.println("Number of leaves:  " +tree.countLeaves(tree.root));
-        System.out.println("Sum of Leaf Nodes: " + tree.findSumOfLeaves(tree.root));
-        System.out.println("Height of the tree is: "+ tree.height(tree.root));
-       // System.out.println("Sum of Node Depths is: " + tree.calculateNodeDepthSums());
-        System.out.println("Sum of all node values is: " + tree.calculateNodeSums());
+
 
     }
 }
