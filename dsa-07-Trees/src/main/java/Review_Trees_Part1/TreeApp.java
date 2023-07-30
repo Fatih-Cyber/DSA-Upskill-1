e3f169074fb0bf81fa3c86b749a981c7fca9859f
Fatih-Cyber <fatih@cybertekschool.com>
2023-07-30 17:14:39 +0200

    Review Trees Insertion

--- dsa-07-Trees/src/main/java/Review_Trees_Part1/TreeApp.java
+++ dsa-07-Trees/src/main/java/Review_Trees_Part1/TreeApp.java
@@ -4,5 +4,9 @@ public class TreeApp {
     public static void main(String[] args) {
         TreeClass tree=new TreeClass();
         System.out.println(tree.root);
+        tree.insert(new TreeNode("John", "locke",5));
+        tree.insert(new TreeNode("Carol", "smith",3));
+        tree.insert(new TreeNode("Betul", "QQ",6));
+        tree.insert(new TreeNode("Kadifa", "WW",4));
     }
 }
--- dsa-07-Trees/src/main/java/Review_Trees_Part1/TreeClass.java
+++ dsa-07-Trees/src/main/java/Review_Trees_Part1/TreeClass.java
@@ -6,4 +6,29 @@ public class TreeClass {
     public TreeClass() {
         //Why default is null? Reference types? Null
     }
+    public void insert(TreeNode newNode){
+        //Tree can be empty
+        if (root==null) {
+            root=newNode;
+            return;
+        }
+        TreeNode current=root;
+        while (true) {
+            if(newNode.id<= current.id){
+                if(current.leftChild==null){
+                    //make insertion
+                    current.leftChild=newNode;//if left is null where you cannot branch left
+                    break;
+                }
+                current=current.leftChild;// if left child is not null branch left
+            }else {
+                if(current.rightChild==null){
+                    //make insertion if right is null where you cannot branch right
+                    current.rightChild=newNode;
+                    break;
+                }
+                current=current.rightChild;// if right child is not null branch right
+            }
+        }
+    }
 }
