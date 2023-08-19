185d72c36d5aa62a9bddf280b8e96012486f033e
Fatih-Cyber <fatih@cybertekschool.com>
2023-08-19 14:28:13 +0200

    Search Review templates pushed

---
+++ dsa-09-Search/src/main/java/SearchReview/Search2DMatrix.java
@@ -0,0 +1,10 @@
+package SearchReview;
+
+public class Search2DMatrix {
+    public static void main(String[] args) {
+
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return false;
+    }
+}
