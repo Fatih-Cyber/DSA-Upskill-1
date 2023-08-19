23a0e91b8ac39e0fa7b9500975c83363f347368f
Fatih-Cyber <fatih@cybertekschool.com>
2023-08-19 17:50:29 +0200

    Search problem 2Dim Array Review templates pushed

--- dsa-09-Search/src/main/java/SearchReview/Search2DMatrix.java
+++ dsa-09-Search/src/main/java/SearchReview/Search2DMatrix.java
@@ -6,9 +6,42 @@ public class Search2DMatrix {
         System.out.println(searchMatrixLogNM(matrix, 3));
     }
     public static boolean searchMatrix(int[][] matrix, int target) {
-     return false;
+        // number of rows and cols
+        int numberOfCols=matrix[0].length;
+        int numberOfRows=matrix.length;
+        // calculate right block
+        int currentRow=0;
+        while(currentRow< numberOfRows-1 ){
+            if(target>matrix[currentRow][numberOfCols-1]) currentRow++;
+            else break;
+        }
+       // binary search in current row
+        int left =0;
+        int right=numberOfCols-1;
+        while (left<=right){
+            int mid=(left+right)/2;
+            if (target==matrix[currentRow][mid]) return true;
+            else if(target<matrix[currentRow][mid]) right=mid-1;
+            else left=mid+1;
+        }
+    return false;
     }
     public static boolean searchMatrixLogNM(int[][] matrix, int target) {
+        int numberOfCols=matrix[0].length;
+        int numberOfRows=matrix.length;
+        int left =0;
+        int right= numberOfRows*numberOfCols-1;
+        while (left<=right){
+            int midIndex=(left+right)/2;
+            // rows and column from index number;
+            int row=midIndex/numberOfCols;
+            int col=midIndex%numberOfCols;
+            // get value from matrix
+            int midValue=matrix[row][col];
+            if (target==midValue) return true;
+            else if(target<midValue) right=midIndex-1;
+            else left=midIndex+1;
+        }
         return false;
     }
 }
--- dsa-09-Search/src/main/java/SearchReview/SearchProblem.java
+++ dsa-09-Search/src/main/java/SearchReview/SearchProblem.java
@@ -2,10 +2,25 @@ package SearchReview;
 
 public class SearchProblem {
     public static void main(String[] args) {
-
+        int[] nums={-1,0,2,4,5,6,7,8};
+        System.out.println(search(nums, 9));
     }
-    public int search(int[] array, int target) {
-    return -1;
+    public static int search(int[] array, int data) {
+        // initialize left and right
+        int left=0;
+        int right=array.length-1;
+        // have a while loop (left<=right)
+        while (left<=right) {
+            // calculate mid
+            int mid=(left+right)/2;
+            // compare == mid then return mid index
+            // else if data<mid then right=mid-1
+            // else left =mid+1
+            if (data==array[mid]) return mid;
+            else if(data<array[mid]) right=mid-1;
+            else left=mid+1;
+        }
+       return -1;
     }
 
 }
