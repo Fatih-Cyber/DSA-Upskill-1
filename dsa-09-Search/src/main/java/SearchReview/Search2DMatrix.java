381de64bef1b429155362572285c78ca360c9381
Fatih-Cyber <fatih@cybertekschool.com>
2023-08-19 15:29:22 +0200

    Search Review templates pushed

--- dsa-09-Search/src/main/java/SearchReview/Search2DMatrix.java
+++ dsa-09-Search/src/main/java/SearchReview/Search2DMatrix.java
@@ -2,9 +2,13 @@ package SearchReview;
 
 public class Search2DMatrix {
     public static void main(String[] args) {
-
+     int[][] matrix={{1,3,5,7},{10,11,16,20},{23,30,34,60}};
+        System.out.println(searchMatrixLogNM(matrix, 3));
     }
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+     return false;
+    }
+    public static boolean searchMatrixLogNM(int[][] matrix, int target) {
         return false;
     }
 }
---
+++ dsa-09-Search/src/main/java/SearchReview/SearchProblem.java
@@ -0,0 +1,11 @@
+package SearchReview;
+
+public class SearchProblem {
+    public static void main(String[] args) {
+
+    }
+    public int search(int[] array, int target) {
+    return -1;
+    }
+
+}
