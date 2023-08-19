f76e9ea5dc332911ece070cca30e0c38df45d781
Fatih-Cyber <fatih@cybertekschool.com>
2023-08-19 14:24:00 +0200

    Review 7 Heapify method added

--- dsa-08-Heap/src/main/java/Review7Heapify/HeapifyTestApp.java
+++ dsa-08-Heap/src/main/java/Review7Heapify/HeapifyTestApp.java
@@ -2,10 +2,9 @@ package Review7Heapify;
 
 public class HeapifyTestApp {
     public static void main(String[] args) {
-        int[] numbers={1,2,5,6,8,21,0,-1,-20,87};
+        int[] numbers={1,2,5,6,8,21,0,-1,-20,87,4,3};
         MyHeap heap=new MyHeap(numbers);
         heap.printHeap();
-
     }
 
 }
