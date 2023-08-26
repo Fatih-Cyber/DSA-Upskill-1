3a3717d9707c86f875d4e5a5ce96f512f21d2d91
Fatih-Cyber <fatih@cybertekschool.com>
2023-08-26 18:34:31 +0200

    LRU cache solved

--- dsa-10-sorting/src/main/java/review_sort/DNode.java
+++ dsa-10-sorting/src/main/java/review_sort/DNode.java
@@ -1,5 +1,16 @@
 package review_sort;
 
-public class DNode {
+public class DNode { // Node class for doubly linked list
+    int key;
+    int value;
+    DNode prev; // Reference type
+    DNode next;
 
+    public DNode(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public DNode() {
+    }
 }
--- dsa-10-sorting/src/main/java/review_sort/LRUCache.java
+++ dsa-10-sorting/src/main/java/review_sort/LRUCache.java
@@ -1,6 +1,73 @@
 package review_sort;
 
-public class LRUCache {
+import java.util.HashMap;
 
+public class LRUCache {
+    int size;
+    int capacity;
+    HashMap<Integer,DNode> cacheMap=new HashMap<>();
+    DNode head;
+    DNode tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.size=0;
+        this.head=new DNode();
+        this.tail=new DNode();
+        head.next=tail;
+        tail.prev=head;
+    }
+    public void addNode(DNode node){
+        node.prev=head;
+        node.next=head.next;
+
+        head.next.prev=node;
+        head.next=node;
+    }
+    public void removeNode(DNode node){
+        DNode prevNode=node.prev;
+        DNode nextNode=node.next;
+
+        prevNode.next=nextNode;
+        nextNode.prev=prevNode;
+    }
+    public void moveToFirst(DNode node){
+        removeNode(node);
+        addNode(node);
+    }
+    public DNode popLast(){
+        DNode popped=tail.prev;
+        removeNode(popped);
+        return popped;
+    }
+    public int get(int key){
+        //Go and check hashmap if you have the key
+        DNode node= cacheMap.get(key);
+        // return -1 if you dont have this key
+        if(node==null) return -1;
+        else {
+            moveToFirst(node);// make it recent
+            return node.value;// return value of node
+        }
+    }
+    public void put(int key, int value){
+        //Go and check hashmap if you have the key
+        DNode node= cacheMap.get(key);
+        if(node==null) { // if I dont have this key in my lru
+            DNode newNode=new DNode(key,value);
+            cacheMap.put(key, newNode); // put in hashmap
+            addNode(newNode); // put in linkedlist
+            size++;
+            if (size>capacity){
+                DNode last=popLast();// remove from the linkedlist
+                cacheMap.remove(last.key);
+                size--;
+            }
+        }else {  // I  have this key in my lru
+            node.value=value; // update the value
+            moveToFirst(node);// make it recent
+
+        }
+    }
 
 }
